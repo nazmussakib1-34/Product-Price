0ca6a7487a09281b0802b45095c558337b83d3ce
unknown <nazmussakib0501200211@gmail.com>
2024-12-07 16:57:05 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='align-middle ml-10 mr-10 mt-6'>
+      <h1 className='text-7xl bg-orange-400 text-center align-middle'>Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
